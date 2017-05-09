29109390407b3e2710137dbb99d595a5820927ac
Jaakko Karhu <jaakko.st.karhu@gmail.com>
2017-05-09 08:35:49 +0200

    combine checkbox and radio button special cases to same function: getDefaultValue --> defaultValue

--- site/src/App.js
+++ site/src/App.js
@@ -6,9 +6,9 @@ const collect = (data) => {
 }
 
 const onChange = (e, data) => {
-  // console.log('App.js', '-->', e.target.value, data);
+  console.log('App.js', '-->', e.target.value, data);
 }
-const value = 'This is mapped value.'
+
 const App = () => (
   /* Non-standard dom attributes are stripped away from inputs in
    * Repeater
@@ -16,7 +16,84 @@ const App = () => (
   <div>
     <Repeater collect={ collect }>
       <h3>Repeated text</h3>
-      <input dataKey='sample-data'
+      <input dataKey='sample-input'
+             onChange={ onChange } />
+      <input type='button'
+             dataKey='sample-button'
+             onChange={ onChange }
+             value='Button' />
+      <input type='checkbox'
+             dataKey='sample-checkbox'
+             value='sample-checkbox-1'
+             onChange={ onChange } />
+      <input type='color'
+             dataKey='sample-color'
+             onChange={ onChange } />
+      <input type='date'
+             dataKey='sample-date'
+             onChange={ onChange } />
+      <input type='datetime-local'
+             dataKey='sample-datetime-local'
+             onChange={ onChange } />
+      <input type='email'
+             dataKey='sample-email'
+             onChange={ onChange } />
+      <input type='file'
+             dataKey='sample-file'
+             onChange={ onChange } />
+      <input type='hidden'
+             dataKey='sample-hidden'
+             onChange={ onChange } />
+      <input type='image'
+             dataKey='sample-image'
+             onChange={ onChange } />
+      <input type='month'
+             dataKey='sample-month'
+             onChange={ onChange } />
+      <input type='number'
+             dataKey='sample-number'
+             onChange={ onChange } />
+      <input type='password'
+             dataKey='sample-password'
+             onChange={ onChange } />
+      <input type='radio'
+             name='sample-radio'
+             dataKey='sample-radio'
+             value='sample-radio-1'
+             onChange={ onChange } />
+      <input type='radio'
+             name='sample-radio'
+             dataKey='sample-radio'
+             value='sample-radio-2'
+             onChange={ onChange } />
+      <input type='range'
+             dataKey='sample-range'
+             onChange={ onChange } />
+      <input type='reset'
+             dataKey='sample-reset'
+             value='Reset button'
+             onChange={ onChange } />
+      <input type='search'
+             dataKey='sample-search'
+             onChange={ onChange } />
+      <input type='submit'
+             dataKey='sample-submit'
+             value='Submit button'
+             onChange={ onChange } />
+      <input type='tel'
+             dataKey='sample-tel'
+             onChange={ onChange } />
+      <input type='text'
+             dataKey='sample-text'
+             onChange={ onChange } />
+      <input type='time'
+             dataKey='sample-time'
+             onChange={ onChange } />
+      <input type='url'
+             dataKey='sample-url'
+             onChange={ onChange } />
+      <input type='week'
+             dataKey='sample-week'
              onChange={ onChange } />
     </Repeater>
   </div>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,71 @@
 /*
  * TODO
  *
- * - Return input value for parent
  * - Test: all types of nested children
- * - Test: passing down data as props will render it
+ * - Collect data structure in init
+ * - Test: returned data structure has to be same as the one which is entered?
+ * - Test special cases for all input prop-types
+ * - Scenario: default values
+ * - Add delete buttons
+ * - Add delete buttons location propType?
+ * - Add githup site for presenting
+ * - Write docs
+ *
+ * SPECIAL CASES
+ *
+ * Non-collectable:
+ *
+ * (Doesn't need a dataKey)
+ * - Button
+ * - Image
+ * - Reset
+ * - Submit
+ * 
+ * Default prop value static:
+ *
+ * - Button
+ * - Checkbox
+ * - Image
+ * - Radio
+ * - Reset
+ * - Submit
+ *
+ * Needs custom default value:
+ *
+ * - Color
+ *
+ * Other, yet undefined special cases:
+ *
+ * - File (returns with fakepath)
+ *
+ * ------------------------------------
  */
 
+import PropTypes from 'prop-types'
+import React from 'react'
 
 const isFunction = (f) => {
   const getType = {}
   return f && getType.toString.call(f) === '[object Function]'
 }
 
-import PropTypes from 'prop-types'
-import React from 'react'
+const getValue = (inputType, propValue, mappedValue) => {
+  switch (inputType) {
+    case 'checkbox':
+    case 'button':
+    case 'image': // Not tested
+    case 'radio':
+    case 'reset':
+    case 'submit':
+      return propValue
+      break
+    case 'color':
+      return mappedValue || '#ffffff'
+      break;
+    default:
+      return mappedValue || ''
+  }
+}
 
 class Repeater extends React.Component {
   static propTypes = {
@@ -37,9 +89,17 @@ class Repeater extends React.Component {
     this.setState({ dataValues: [ ...this.state.dataValues, {} ] })
   }
 
-  onChange = (e, index, dataKey, cb) => {
+  onChange = (e, index, dataKey, inputType, cb) => {
+    /* Make switch statement here to check the type,
+     * store input value accordingly
+     *
+     */
     const nDataValues = [ ...this.state.dataValues ]
-    nDataValues[index][dataKey] = e.target.value
+    if (inputType=='checkbox'&&!e.target.checked) {
+      nDataValues[index][dataKey] = ''
+    } else {
+      nDataValues[index][dataKey] = e.target.value // Assign here instead of reading the e directly
+    }
     this.setState({ dataValues: nDataValues })
     if (isFunction(cb)) cb(e, nDataValues)
   }
@@ -55,10 +115,15 @@ class Repeater extends React.Component {
         propsCp.children = copyChildren(propsCp.children)
         // Check the case for arrays as well?
         if (child.type=='input'&&!dataKey) {
+          // Note that child type is different than input type passed as prop
+          // Print details about the child, to make it more easy to find
           console.warn('[react-repeater]:Input is missing dataKey. Data cannot be mapped to state properly. Please add dataKey prop to child element.')
         } else if (child.type=='input') {
-          propsCp.onChange = (e) => this.onChange(e, index, dataKey, onChange)
-          propsCp.value = dataValues[index][dataKey] || ''
+          const inputType = propsCp.type,
+                { value } = propsCp,
+                nValue = getValue(inputType, value, dataValues[index][dataKey])
+          propsCp.onChange = (e) => this.onChange(e, index, dataKey, inputType, onChange)
+          propsCp.value = nValue
         }
         delete propsCp.dataKey // Not sure if this is a good idea instead of using data-key
         return child.type
