b350eea04ecc7c407bf8e227a16f1c475ac0f97f
Jaakko Karhu <jaakko.st.karhu@gmail.com>
2017-09-09 16:37:00 +0200

    Remove unnecessary logging

--- site/src/utilities/getIntendedJSON.js
+++ site/src/utilities/getIntendedJSON.js
@@ -9,7 +9,6 @@ for(var a=0; a < splitted.length; a++) {
     if(!!str) arr.push(<span>{str}</span>, <br />)
         if(a!=splitted.length-1) arr.push('\u00a0\u00a0')
     }
-    console.log('ARR', arr)
     return arr
 }
 
